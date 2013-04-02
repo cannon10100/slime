ed1e3dbf82e675fd38f8a97d4fe628c43208fbf0
cannon10100 <cannon10100@gmail.com>
2013-04-01 19:21:39 -0500

    Added ideas directory

---
+++ src/main/java/ideas/src/BlockTypes.java
@@ -0,0 +1,28 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: s619532
+ * Date: 3/18/13
+ * Time: 3:27 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum BlockTypes implements EnumConverter {
+    AIR(0),
+    WATER(1),
+    ;
+
+    private final int id;
+
+    public int getId() {
+
+        return id;
+    }
+
+    private BlockTypes(int id) {
+        this.id = id;
+
+    }
+
+    public int convert() {
+        return id;
+    }
+}
